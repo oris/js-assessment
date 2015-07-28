f110e81c8908deb05aa03a862f4e867d944a252c
Benjamin Oris Jr <bcoris@gmail.com>
2015-07-29 04:35:17 +0800

    Locate an item in an array

--- app/arrays.js
+++ app/arrays.js
@@ -3,7 +3,10 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    for ( var i = 0, len = arr.length; i < len; i++ ) {
+      if ( arr[i] === item ) return i;
+    };
+    return -1;
   },
 
   sum : function(arr) {
