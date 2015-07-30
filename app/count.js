79d46119e557f2fd3ba24a0c323df5125d83f05e
Benjamin Oris Jr <bcoris@gmail.com>
2015-07-30 22:25:41 +0800

    Count with 1/10th of a second interval

--- app/count.js
+++ app/count.js
@@ -2,6 +2,19 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var num = start;
 
+    var countNow = function countOn() {
+      if (num > end) {
+        return;
+      };
+      console.log(num);
+
+      setTimeout(function () {
+        num++;
+        countOn()}, 100);
+    };
+
+    countNow();
   }
 };
