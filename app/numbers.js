ebb674ff72473ae6ce872c8e38a788287cfe60c8
Benjamin Oris Jr <bcoris@gmail.com>
2015-08-07 06:36:07 +0800

    Do all sorts of things with numbers

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,29 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
+    var mask = Math.pow(2, bit - 1);
 
+    return (mask & num) ? 1 : 0;
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
+    var bits = 8,
+      binaryNumStr = num.toString(2);
 
+    return '0'.repeat(bits - binaryNumStr.length) + binaryNumStr;
   },
 
   multiply: function(a, b) {
+    function getPrecision(num) {
+      return num.toString().length - 2;
+    }
+    var prec = (getPrecision(a) > getPrecision(b)) ?
+                    getPrecision(a) : getPrecision(b);
 
+    return parseFloat((a * b).toPrecision(prec));
   }
 };
