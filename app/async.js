a7b3a7aa4de76f2689554c7fb76b2b91ed8e6465
Benjamin Oris Jr <bcoris@gmail.com>
2015-08-09 21:49:27 +0800

    Know how to use promises

--- app/async.js
+++ app/async.js
@@ -2,7 +2,9 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return new Promise(function(fulfill) {
+      fulfill(value);
+    });
   },
 
   manipulateRemoteData : function(url) {
